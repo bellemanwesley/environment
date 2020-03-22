452b26ed084e1b6eed627d92dea9c1cc1a541e9b
Wesley Belleman <wesley.belleman.2@us.af.mil>
2020-03-22 09:02:30 -0700

    added homepage

--- server_deploy.py
+++ server_deploy.py
@@ -3,8 +3,13 @@ from flask_restful import Resource, Api
 import json
 from multiprocessing import Process
 
+#Might need this: https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+
 deployment_service = Flask(__name__)
 api = Api(deployment_service)
+master_file = open('ignore_files/word_files/master.json','r')
+master_dict = json.loads(master_file.read())
+master_file.close()
 
 class welcome(Resource):
 	def get(self):
@@ -13,22 +18,15 @@ class welcome(Resource):
 class deploy(Resource):
 	def post(self):
 		data = request.get_json()
-		print(type(data))
-		try:
-			success = True
-		except:
-			success = False
-
-		if success:
-			response = {
-				'status': 200,
-				'message': 'Deployment Successful',
-			}
-		else:
-			response = {
-				'status': 700,
-				'message': 'Deployment Failed'
-			}
+		data_dict = json.loads(data)
+		my_key = data_dict['key']
+		i = 1
+		response = {}
+		while str(i) in data_dict:
+			try:
+				response.update(str(i):master_dict[str(i)][my_key])
+			except:
+				response.update(str(i):data_dict[str(i)])
 		return jsonify(response)
 
 api.add_resource(welcome, '/')
