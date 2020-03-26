58417417d00bb7c77d4389ae1d08b5d2757d0dca
Wesley Belleman <wesley.belleman.2@us.af.mil>
2020-03-26 06:57:11 -0700

    Added article to environment

--- website/ukrainian/accent_tool.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-r = requests.get('https://search-dcos-tutorial-2kfa5db5yllkqpnfaysztenoku.us-east-2.es.amazonaws.com/movies/_search?q=mars')
-with f as open("temp_file","w+"):
-	f.write(r.text)
